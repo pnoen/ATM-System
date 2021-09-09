d1066942e55fe8a855719b59412049bd9a00581d
Imran Issa <iiss3448@uni.sydney.edu.au>
2021-09-09 21:25:03 +1000

    Update Card.java

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,7 +1,38 @@
-
+import java.util.Date;
 
 public class Card {
-
+    private int Num;
+    private int PIN;
+    private boolean isLost;
+    private Date issueDate = new Date();
+    private Date expiryDate = new Date();
+    private int attempts;
+    
+    public Card(int n, int p, Date iDate, Date eDate) {
+        this.Num = n;
+        this.PIN = p;
+        this.isLost = False;
+        this.issueDate = iDate;
+        this.expiryDate = eDate;
+        this.attempts = 0;
+    }
+    
+    public void setPIN(int p) {
+        this.PIN = p;
+    }
+    
+    public int getPIN() {
+        return this.PIN;
+    }
+    
+    public int getAttempts() {
+        return this.attempts;
+    }
+    
+    public void setAttempts(int attempt) {
+        this.attempts += attempt;
+    }
+    
     public boolean isValid(){
         //whether the card entered is a valid card number
         return true;
