ebd0228a5db943f90d9d86eed7db2b57ec9468fe
mkar6206 <mkar6206@uni.sydney.edu.au>
2021-09-13 23:17:22 +1000

    update

--- src/main/java/ATM.java
+++ src/main/java/ATM.java
@@ -390,7 +390,7 @@ public class ATM {
     
     public static void main(String[] args) throws ParseException {
         // Create the atm and start it.
-        ATM atm = new ATM("01/01/2021", "cards.csv", 100000);
+        ATM atm = new ATM("01/01/2021", "cards.csv", 199934690);
         atm.readCSV();
         Admin admin = new Admin(atm, 99999, 9999);
         Scanner cardInput = new Scanner(System.in);
