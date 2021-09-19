80bb27f3c61a7ac390af6880092424e90a8868f3
Will <wqiu8445@uni.sydney.edu.au>
2021-09-19 17:32:38 +1000

    deposit and withdraw tests

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,17 +1,72 @@
-
+import java.util.Date;
 
 public class Card {
+    private int cardID; // Account Number
+    private int pin; // Account PIN Number
+    private int currBalance;
+    private boolean isLost; // Is the card lost or stolen
+    private boolean isBlocked;
+    private String fullname;
+    private Date issueDate; // Date of issue
+    private Date expiryDate; // Expiry date
 
-    public boolean isValid(){
-        //whether the card entered is a valid card number
-        return true;
+    //Sets all the variables
+    public Card(int cardID, String fullname, int pin, int currBalance, Date issueDate, Date expiryDate, boolean isLost) {
+        this.cardID = cardID;
+        this.pin = pin;
+        this.fullname = fullname;
+        this.currBalance = currBalance;
+        this.isLost = isLost;
+        this.issueDate = issueDate;
+        this.expiryDate = expiryDate;
+        this.isBlocked = false;
     }
 
-    public boolean isLost(){
-        //whether the card was reported stolen or lost
-        return false;
+    ///Returns the name of the Card holder
+    public String getFullname(){return this.fullname; }
+
+    //Returns the card number
+    public int getAccNo() {
+        return this.cardID;
     }
 
+    //If the pin fails 3 times, this fucntion sets the block state when needed
+    public void setBlockState(boolean state){
+        this.isBlocked = state;
+    }
 
+    //Returns the block state
+    public boolean getBlockState(){
+        return this.isBlocked;
+    }
 
+    //Returns the issue date of the card
+    public Date getIssueDate() {
+        return this.issueDate;
+    }
+
+    //Returns the expiry date of the card
+    public Date getExpiryDate() {
+        return this.expiryDate;
+    }
+
+    //Returns the lost/stolen state of the Card
+    public boolean getIsLost() {
+        return this.isLost;
+    }
+
+    //Returns the pin for the card
+    public int getPIN() {
+        return this.pin;
+    }
+
+    //Sets the balance of the card
+    public void setCurrBalance(int newBalance){
+        this.currBalance = newBalance;
+    }
+
+    //Gets the current balance of the card
+    public int getCurrBalance() {
+        return this.currBalance;
+    }
 }
--- src/test/java/TestATM.java
+++ src/test/java/TestATM.java
@@ -1,16 +1,66 @@
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.util.Date;
 
 public class TestATM {
 
     @Test
-    void testRandom() {
-        ATM test = new ATM();
-        String printer = test.noFunds();
-        //test.add where add is the method created in the JUnitDemo class
-        assertEquals("Not enough money inside ATM", printer);
+    void testConstructor() {
+        ATM test = new ATM("Hello", "test_cards", 100000);
     }
 
+    @Test
+    void testFileReader() throws ParseException {
 
+        ATM valid_atm = new ATM("01/01/2021", "cards.csv", 199934690);
+        ATM invalid_atm = new ATM("01/01/2021", "fakecards.csv", 199934690);
+
+        //Assertions.assertThrows(FileNotFoundException.class, ()-> invalid_atm.readCSV());
+        //assertEquals("Could not load the database", valid_atm.readCSV());
+    }
+
+    @Test
+    void testCardCreation() throws ParseException{
+        ATM valid_atm = new ATM("01/01/2021", "cards.csv", 199934690);
+        Card test_card = valid_atm.createCard("123", "alex", "123", "0", "01/12/2020", "10/12/2020", "false");
+        assertEquals("alex", test_card.getFullname());
+        assertEquals(0, test_card.getCurrBalance());
+        assertEquals(123, test_card.getAccNo());
+        assertEquals(123, test_card.getPIN());
+        assertEquals(false, test_card.getIsLost());
+    }
+
+    @Test
+    void testAdminCreation(){
+        ATM test_atm = new ATM("01/01/2021", "cards.csv", 10);
+        Admin admin = new Admin(test_atm, 99999, 9999);
+        assertEquals(99999, admin.getID());
+        assertEquals(9999, admin.getPin());
+        admin.addFunds(10);
+        assertEquals(20, test_atm.getBalanceATM());
+
+    }
+
+    @Test
+    void testBalance() throws ParseException{
+        ATM valid_atm = new ATM("01/01/2021", "cards.csv", 10);
+        valid_atm.readCSV();
+        Admin admin = new Admin(valid_atm, 99999, 9999);
+        assertEquals(10, valid_atm.getBalanceATM());
+
+    }
+
+    @Test
+    void testCheckValid() throws ParseException{
+        ATM valid_atm = new ATM("01/01/2021", "cards.csv", 10);
+        valid_atm.readCSV();
+        assertTrue(valid_atm.checkValid(00006));
+        assertFalse(valid_atm.checkValid(0055));
+    }
 }
+
+
